c4355f7f0769cf974394b845665e16b166433f16
Matias Alvin <mat@matias-laptop>
2026-01-12 18:54:50 +0700

    Fix: add fix for payload creation

--- curl_grpc.cc
+++ curl_grpc.cc
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 
 // curl stuff
 #include <curl/curl.h>
@@ -48,62 +49,32 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  std::cout << argv[2] << "\n";
-
   google::protobuf::compiler::MultiFileErrorCollector *error_collector = new SimpleErrorCollector();
   google::protobuf::compiler::DiskSourceTree *source_tree = new google::protobuf::compiler::DiskSourceTree();
   source_tree->MapPath("", ".");
 
   google::protobuf::compiler::Importer *importer = new google::protobuf::compiler::Importer(source_tree, error_collector);
   const google::protobuf::FileDescriptor *fd = importer->Import(argv[1]);
-  std::cout << "sucess load protofile: " << fd->name() << "\n";
 
   google::protobuf::util::TypeResolver *type_resolver = google::protobuf::util::NewTypeResolverForDescriptorPool("type.googleapis.com", fd->pool());
   std::string payload;
   google::protobuf::util::Status status = google::protobuf::util::JsonToBinaryString(type_resolver, "type.googleapis.com/helloworld.HelloRequest", argv[2], &payload);
-  if (!status.ok())
-    std::cout << "fail to parse json to message: " << status.error_message() << "\n";
+  if (!status.ok()) {
+    std::cout << "fail to parse json to proto message\n";
+    return 1;
+}
 
-  // std::cout << "payload length: " << payload.length() << "\n";
-
-  // std::string payload_json;
-  // google::protobuf::util::Status status_second = google::protobuf::util::BinaryToJsonString(type_resolver, "type.googleapis.com/helloworld.HelloRequest", payload, &payload_json);
-  // if (!status_second.ok())
-  //   std::cout << "fail to parse message to json: " << status.error_message() << "\n";
-
-  // std::cout << "json payload: " << payload_json << "\n";
-
-  // char command[255];
-  // sprintf(command, "protoc --encode=helloworld.HelloRequest %s > encoded_payload", argv[1]);
-  // FILE *encode_pipe = popen(command, "w");
-  // fputs(argv[2], encode_pipe);
-  // pclose(encode_pipe);
-
-  // struct stat st;
-  // stat("encoded_payload", &st);
-  // size_t len = st.st_size;
   size_t len = payload.length();
-  // uint8_t *buf;
-  // buf = (uint8_t *) malloc(PREFIX_LENGTH);
+  std::vector<uint8_t> buf = {0, 0, 0, 0, 0};
+  buf.insert(buf.end(), payload.begin(), payload.end());
 
-  // buf[0] = 0;
-  std::string buf;
   int *bin = dec_to_bin(len, 32);
   for (int idx = 0; idx < 4; idx++) {
     long num = bin_to_dec(bin + idx * 8, 8);
-    // buf[4 - idx] = num;
-    buf.insert(0, 1, num);
+    buf[4 - idx] = num;
   }
-  std::cout << "buf: " << buf << "\n";
-  // free(bin);
-  // strncpy((char *)buf+len, payload.c_str(), len);
-  std::string payload_buf = buf + payload;
+  free(bin);
 
-  // FILE *encoded_payload = fopen("encoded_payload", "rb");
-  // fread(buf+PREFIX_LENGTH, len,1, encoded_payload);
-  // fclose(encoded_payload);
-
-  std::cout << "payload: " << payload_buf << "\n";
   curl_global_init(CURL_GLOBAL_ALL);
   CURL *curl = curl_easy_init();
   if (!curl) {
@@ -116,13 +87,13 @@ int main(int argc, char **argv) {
                    "http://localhost:50051/helloworld.Greeter/SayHello");
   curl_easy_setopt(curl, CURLOPT_HTTP_VERSION,
                    CURL_HTTP_VERSION_2_PRIOR_KNOWLEDGE);
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload_buf.c_str());
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buf.data());
   curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, len + PREFIX_LENGTH);
   struct curl_slist *headers = NULL;
   headers = curl_slist_append(headers, "Content-Type: application/grpc+proto");
   curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handle_callback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, argv[1]);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, fd->pool());
 
   CURLcode res = curl_easy_perform(curl);
   if (res != CURLE_OK)
@@ -131,9 +102,6 @@ int main(int argc, char **argv) {
 
   curl_easy_cleanup(curl);
   curl_global_cleanup();
-
-  std::cout << "here\n";
-  // free(buf);
 
   return 0;
 }
@@ -156,11 +124,14 @@ long bin_to_dec(int *bin, size_t len) {
 }
 
 size_t handle_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
-  char command[255];
-  sprintf(command, "protoc --decode=helloworld.HelloReply %s > response_decoded", (char *) userdata);
-  FILE *decode_pipe = popen(command, "w");
-  fputs(ptr+PREFIX_LENGTH, decode_pipe);
-  pclose(decode_pipe);
+  std::string payload_json;
+  google::protobuf::util::TypeResolver *type_resolver = google::protobuf::util::NewTypeResolverForDescriptorPool("type.googleapis.com", (google::protobuf::DescriptorPool *) userdata);
+  google::protobuf::util::Status status_second = google::protobuf::util::BinaryToJsonString(type_resolver, "type.googleapis.com/helloworld.HelloReply", ptr+PREFIX_LENGTH, &payload_json);
+
+  if (!status_second.ok())
+    std::cout << "fail to parse message to json\n";
+  else
+    std::cout << "response: " << payload_json << "\n";
 
   return size * nmemb;
 }
